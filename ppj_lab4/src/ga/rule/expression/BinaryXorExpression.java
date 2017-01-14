f679bbb3f16f37af14e66150fe342c334c609d41
Dominik Stanojevic <dominik.stanojevic@fer.hr>
2017-01-14 21:09:25 +0100

    10. testni riješen

--- ppj_lab4/src/ga/rule/expression/BinaryXorExpression.java
+++ ppj_lab4/src/ga/rule/expression/BinaryXorExpression.java
@@ -4,6 +4,7 @@ import ga.Environment;
 import ga.Property;
 import ga.PropertyType;
 import ga.SemanticException;
+import ga.SemantickiAnalizator;
 import ga.Types;
 import ga.node.NonTerminalNode;
 import ga.rule.RuleStrategy;
@@ -15,8 +16,7 @@ public class BinaryXorExpression extends RuleStrategy {
     public void evaluate(NonTerminalNode node, Environment environment) {
         if (node.getChildNodeNumber() == 1) {
             node.getChidlAt(0).visitNode(environment);
-            node.setProperty(PropertyType.TYPE,
-                    ((NonTerminalNode) node.getChidlAt(0)).getProperty(PropertyType.TYPE));
+            node.setProperty(PropertyType.TYPE, ((NonTerminalNode) node.getChidlAt(0)).getProperty(PropertyType.TYPE));
             node.setProperty(PropertyType.L_EXPRESSION,
                     ((NonTerminalNode) node.getChidlAt(0)).getProperty(PropertyType.L_EXPRESSION));
         } else if (node.getChildNodeNumber() == 3) {
@@ -42,7 +42,17 @@ public class BinaryXorExpression extends RuleStrategy {
         if (node.getChildNodeNumber() == 1) {
             node.getChidlAt(0).visitNode(environment);
         } else {
-            //TODO: 3 children
+            node.getChidlAt(0).visitNode(environment);
+            node.getChidlAt(2).visitNode(environment);
+
+            if (!SemantickiAnalizator.init) {
+                System.out.println("\t POP R1");
+                System.out.println("\t POP R0");
+                System.out.println("\t XOR R0, R1, R0");
+                System.out.println("\t PUSH R0");
+            } else {
+                //TODO
+            }
         }
     }
 }
